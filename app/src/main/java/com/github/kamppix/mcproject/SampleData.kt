6cc172ca3c65c6e71f9c44093c17ef14eaed0b84
Kamppix <kimmo.leukkunen@gmail.com>
2025-01-21 10:44:15 +0200

    Initial commit

---
+++ app/src/main/java/com/github/kamppix/mcproject/SampleData.kt
@@ -0,0 +1,79 @@
+import com.github.kamppix.mcproject.Message
+
+/**
+ * SampleData for Jetpack Compose Tutorial 
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Kamppi",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Kamppi",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Kamppi",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Kamppi",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Kamppi",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Kamppi",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Kamppi",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Kamppi",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Kamppi",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Kamppi",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Kamppi",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Kamppi",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Kamppi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+        Message(
+            "Kamppi",
+            "This is a test text. Please do not read the test text."
+        ),
+    )
+}
